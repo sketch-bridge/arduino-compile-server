5643c8c49929aaf5ce7240afa23e722a70991131
Yoichiro Tanaka <yoichiro6642@gmail.com>
2025-03-19 06:30:14 +0900

    Support Raspberry Pi Pico and Raspberry Pi Pico W.

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,12 @@ RUN apt-get install -y build-essential wget curl zip lsb-release git ssh
 
 RUN curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/usr/local/bin sh
 
+RUN arduino-cli config add board_manager.additional_urls https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
 RUN arduino-cli core update-index
 RUN arduino-cli core install arduino:avr
 RUN arduino-cli core install arduino:renesas_uno
+RUN arduino-cli core install rp2040:rp2040
+RUN arduino-cli config dump --verbose
 
 WORKDIR /app
 
--- common/arduino.go
+++ common/arduino.go
@@ -1,8 +1,9 @@
 package common
 
 type Platform struct {
-	Name    string
-	Version string
+	Name          string
+	Version       string
+	AdditionalUrl string
 }
 
 type Board struct {
@@ -12,13 +13,21 @@ type Board struct {
 }
 
 var ArduinoAvr = Platform{
-	Name:    "arduino:avr",
-	Version: "1.8.6",
+	Name:          "arduino:avr",
+	Version:       "1.8.6",
+	AdditionalUrl: "",
 }
 
 var ArduinoRenesasUno = Platform{
-	Name:    "arduino:renesas_uno",
-	Version: "1.3.2",
+	Name:          "arduino:renesas_uno",
+	Version:       "1.3.2",
+	AdditionalUrl: "",
+}
+
+var Rp2040 = Platform{
+	Name:          "rp2040:rp2040",
+	Version:       "4.5.0",
+	AdditionalUrl: "https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json",
 }
 
 var Boards = map[string]Board{
@@ -31,5 +40,15 @@ var Boards = map[string]Board{
 		Platform: ArduinoRenesasUno,
 		Fqbn:     "arduino:renesas_uno:minima",
 		Exts:     []string{"hex", "elf", "bin"},
+	},
+	"rp2040:rp2040:rpipico": {
+		Platform: Rp2040,
+		Fqbn:     "rp2040:rp2040:rpipico",
+		Exts:     []string{"bin", "elf", "uf2"},
+	},
+	"rp2040:rp2040:rpipicow": {
+		Platform: Rp2040,
+		Fqbn:     "rp2040:rp2040:rpipicow",
+		Exts:     []string{"bin", "elf", "uf2"},
 	},
 }
